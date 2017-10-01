5928f6f44715738b949272b07055535a2917ba69
Joe Kramer <joekramer1125@gmail.com>
2017-10-01 01:52:22 -0700

    logic working, need to implement minimax algorithm

--- js/scripts.js
+++ js/scripts.js
@@ -13,40 +13,65 @@ const WINCOMBOS = [
 ];
 
 var board;
-var gameWon;
 startGame()
 function startGame() {
   $(".cell").text("");
   board = ["", "", "", "", "", "", "", "", ""];
-  gameWon = false;
 }
 
 function turn(cellID, player) {
   if ($("#" + cellID).text() === "") {
     $("#" + cellID).text(player);
     board[cellID] = player;
-    gameWon = checkWin(board, player);
+    checkWin(board, player);
   } else {
     alert("Space already marked " + $("#" + cellID).text());
   }
 }
 
 function checkWin(board, player) {
-  loop:
   for (var i = 0; i < WINCOMBOS.length; i++) {
     var counter = 0;
     for (var j = 0; j < WINCOMBOS[i].length; j++) {
       if (board[WINCOMBOS[i][j]] === player) {
         counter++;
         if (counter === WINCOMBOS[i].length) {
+          gameOver(WINCOMBOS[i], player);
           console.log("true");
-          return true;
+          return;
         }
       }
     }
   }
   console.log("false");
-  return false;
+  return;
+}
+
+function gameOver(winningCombo, player) {
+  for (var i = 0; i < winningCombo.length; i++) {
+    if (player === "X") {
+      $("#" + winningCombo[i]).addClass("blueWin");
+    } else {
+      $("#" + winningCombo[i]).addClass("redWin");
+    }
+  }
+}
+
+function checkTie() {
+  for (var cell in board) {
+    if (cell === "") {
+      return false;
+    }
+  }
+}
+
+function minimax() {
+  for(var i = 0; i < board.length; i++) {
+    if (board[i] === "") {
+      turn(i, AIPLAYER);
+      return;
+    }
+  }
 }
 
 // jQuery/frontend
@@ -57,6 +82,11 @@ $(document).ready(function() {
 
   $("table").on('click', '.cell', function(event) {
     turn(event.currentTarget.id, HUMANPLAYER);
+    if (!checkTie()) {
+      turn(minimax(), AIPLAYER);
+    } else {
+      alert("TIE");
+    }
   });
 
 
